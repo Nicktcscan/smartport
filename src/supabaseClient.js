62bbd8d8d93e282a6c307644ef881c37e47b57b5
MadsoftTech <madsofttechnologies@gmail.com>
2025-10-24 19:38:39 +0000

    Live site updated

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,9 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL || 'https://trjwjsfcebucwrfeewyt.supabase.co';
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL || 'https://cgyjradpttmdancexdem.supabase.co';
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY || 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyandqc2ZjZWJ1Y3dyZmVld3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MTAxNjQsImV4cCI6MjA3NDI4NjE2NH0.a_W5Ke9AvKhBLDSQNJ2uxP4N4lbP1rrZb-iHjOedopk';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNneWpyYWRwdHRtZGFuY2V4ZGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NTkzMzcsImV4cCI6MjA3MDEzNTMzN30.A9ps6I1p-GC7Anj1mLoLUy_c1sRXXZ_o3EaMDFT1Tcw';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   realtime: {
