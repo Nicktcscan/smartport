57c62b35b5d55731d1a995a416db7d6096095a8c
MadsoftTech <madsofttechnologies@gmail.com>
2025-10-24 19:24:49 +0000

    Cleanup live repo for migration

--- src/pages/customs/AuditTrail.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/customs/AuditTrail.jsx
-import React from 'react';
-
-function AuditTrail() {
-  return <h1>Audit Trail of Activities</h1>;
-}
-
-export default AuditTrail;
--- src/pages/customs/MovementLogs.jsx
+++
@@ -1,8 +0,0 @@
-// src/pages/customs/MovementLogs.jsx
-import React from 'react';
-
-function MovementLogs() {
-  return <h1>Vehicle Movement Logs</h1>;
-}
-
-export default MovementLogs;
